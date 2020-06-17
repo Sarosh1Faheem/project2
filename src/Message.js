1967198a64f6b696df350b33387d223b1c0cf01c
Sarosh Faheem <52979134+Sarosh1Faheem@users.noreply.github.com>
2020-06-18 02:31:17 +0500

    Pushing Project On GitHub

---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Message(props) {
+   return(
+    <h3 className="DayLight">Hello, World.! This is React Application "Class {props.counter}" </h3>
+    )
+}
